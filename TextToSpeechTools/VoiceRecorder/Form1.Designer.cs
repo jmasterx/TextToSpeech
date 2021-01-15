1c003be474bb4d82eac5c013b9d1f7da1bdf81a0
joshua_larouche@hotmail.com <joshua_larouche@hotmail.com>
2021-01-14 19:08:48 -0500

    Add voice recorder

---
+++ TextToSpeechTools/VoiceRecorder/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace VoiceRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecord.Location = new System.Drawing.Point(317, 830);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(218, 163);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.BtnRecord_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(598, 830);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(218, 163);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1430, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(164, 784);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(56, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1357, 784);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(1184, 830);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(218, 163);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(877, 830);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(218, 163);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.BtnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1611, 1004);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRecord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ TextToSpeechTools/VoiceRecorder/Form1.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+using System.Windows.Forms;
+
+namespace VoiceRecorder
+{
+    public partial class Form1 : Form
+    {
+        private string CurrentFile = "test.wav";
+        private string Normalized = "Normalized";
+        private string Stereo = "Stereo";
+        private string Raw = "Raw";
+        private string Trimmed = "Trimmed";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public WasapiCapture capture;
+        public WaveFileWriter waveFile = null;
+
+        void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+
+        void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (capture != null)
+            {
+                capture.Dispose();
+                capture = null;
+            }
+
+            if (waveFile != null)
+            {
+                waveFile.Dispose();
+                waveFile = null;
+            }
+
+            StereoToMono();
+            Normalize();
+            StripSilence();
+
+            btnRecord.Enabled = true;
+        }
+
+        private void BtnRecord_Click(object sender, EventArgs e)
+        {
+            if (CurrentFile == null)
+                return;
+
+            File.Delete($"{Stereo}/{CurrentFile}");
+            File.Delete($"{Raw}/{CurrentFile}");
+            File.Delete($"{Trimmed}/{CurrentFile}");
+            File.Delete($"{Normalized}/{CurrentFile}");
+
+            capture = new WasapiCapture();
+            btnRecord.Enabled = false;
+            btnStop.Enabled = true;
+
+            capture.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+            capture.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+
+            waveFile = new WaveFileWriter($"{Stereo}/{CurrentFile}", capture.WaveFormat);
+
+            capture.StartRecording();
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = false;
+
+            capture.StopRecording();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var sox = ConfigurationSettings.AppSettings.Get("Sox");
+
+            if (!File.Exists(sox))
+            {
+                MessageBox.Show(
+                    $"{sox} was not found and is required to trim silence. Get it from https://sourceforge.net/projects/sox/ and update VoiceRecorder.exe.config to point to its installation folder.");
+            }
+            btnStop.Enabled = false;
+            Directory.CreateDirectory(Trimmed);
+            Directory.CreateDirectory(Raw);
+            Directory.CreateDirectory(Normalized);
+            Directory.CreateDirectory(Stereo);
+
+            listBox1.SelectedIndexChanged += (send, args) =>
+            {
+                CurrentFile = ((WorkItem)listBox1.SelectedItem).Filename;
+                textBox1.Text = ((WorkItem)listBox1.SelectedItem).Text;
+            };
+
+            using (TextFieldParser parser = new TextFieldParser(@"Data\metadata.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters("|");
+                parser.HasFieldsEnclosedInQuotes = true;
+                while (!parser.EndOfData)
+                {
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    var item = new WorkItem
+                    {
+                        Filename = fields[0] + ".wav",
+                        Text = fields[1],
+                    };
+
+                    listBox1.Items.Add(item);
+
+                    if(File.Exists($"{Stereo}/{item.Filename}"))
+                    {
+                        listBox1.SelectedItem = item;
+                    }
+                }
+            }
+        }
+
+        public void StereoToMono()
+        {
+            var inPath = $"{Stereo}/{CurrentFile}";
+            var outPath = $"{Raw}/{CurrentFile}";
+            using (var inputReader = new AudioFileReader(inPath))
+            {
+                // convert our stereo ISampleProvider to mono
+                var mono = new StereoToMonoSampleProvider(inputReader);
+                mono.LeftVolume = 1.0f; // discard the left channel
+                mono.RightVolume = 0.0f; // keep the right channel
+
+                // ... OR ... could write the mono audio out to a WAV file
+                WaveFileWriter.CreateWaveFile16(outPath, mono);
+            }
+        }
+
+        private void Normalize()
+        {
+            var inPath = $"{Raw}/{CurrentFile}";
+            var outPath = $"{Normalized}/{CurrentFile}";
+            float max = 0;
+
+            using (var reader = new AudioFileReader(inPath))
+            {
+                // find the max peak
+                float[] buffer = new float[reader.WaveFormat.SampleRate];
+                int read;
+                do
+                {
+                    read = reader.Read(buffer, 0, buffer.Length);
+                    for (int n = 0; n < read; n++)
+                    {
+                        var abs = Math.Abs(buffer[n]);
+                        if (abs > max) max = abs;
+                    }
+                } while (read > 0);
+                Console.WriteLine($"Max sample value: {max}");
+
+                if (max == 0 || max > 1.0f)
+                    throw new InvalidOperationException("File cannot be normalized");
+
+                // rewind and amplify
+                reader.Position = 0;
+                reader.Volume = 1.0f / max;
+
+                // write out to a new WAV file
+                WaveFileWriter.CreateWaveFile16(outPath, reader);
+            }
+        }
+
+        private void StripSilence()
+        {
+            var inPath = $"{Normalized}/{CurrentFile}";
+            var outPath = $"{Trimmed}/{CurrentFile}";
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = ConfigurationSettings.AppSettings.Get("Sox");
+            startInfo.Arguments = ConfigurationSettings.AppSettings.Get("SoxArgs").Replace("inPath", inPath).Replace("outPath", outPath);
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = false;
+            startInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            using (Process soxProc = Process.Start(startInfo))
+            {
+                soxProc.WaitForExit();
+            }
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            if(!File.Exists($"{Trimmed}/{CurrentFile}"))
+            {
+                MessageBox.Show("No recording");
+                return;
+            }
+
+            btnPlay.Enabled = false;
+            using (WaveStream mainOutputStream = new WaveFileReader($"{Trimmed}/{CurrentFile}"))
+            {
+                using (WaveChannel32 volumeStream = new WaveChannel32(mainOutputStream))
+                {
+                    volumeStream.PadWithZeroes = false;
+
+                    using (WaveOutEvent player = new WaveOutEvent())
+                    {
+                        player.Init(volumeStream);
+
+                        player.Play();
+                        while(player.PlaybackState == PlaybackState.Playing)
+                        {
+                            Thread.Sleep(1);
+                        }
+                        player.Dispose();
+                    }
+                }
+            }
+            btnPlay.Enabled = true;
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex++;
+        }
+    }
+}
